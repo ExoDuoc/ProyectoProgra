02528949d53dbf97afeab0d542756feaedc0d322
ExoDuoc <byro.gallardo@duocuc.cl>
2025-01-18 13:51:54 -0300

    four commit

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -7,33 +7,47 @@ import { UsuarioService } from '../api/usuario.service';
   styleUrls: ['./registro.page.scss'],
 })
 export class RegistroPage implements OnInit {
+  user: any = {
+    nombre: '',
+    email: '',
+    password: '',
+    f_nacimiento: '',
+    genero: '',
+  };
 
-  user:any={
-    nombre:'',
-    email:'',
-    password:'',
-    f_nacimiento:'',
-    genero:''
-  }
+  error: boolean = false; // Se usa para mostrar mensajes de error
 
-  constructor(private usuarioService:UsuarioService) { }
+  constructor(private usuarioService: UsuarioService) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  registrar(){
+  registrar() {
+    // Verificar si hay campos vacíos
+    if (
+      !this.user.nombre ||
+      !this.user.email ||
+      !this.user.password ||
+      !this.user.f_nacimiento ||
+      !this.user.genero
+    ) {
+      this.error = true; // Activa los mensajes de error si hay campos vacíos
+      return;
+    }
+
+    this.error = false; // Limpia el estado de error si todos los campos están completos
+
     console.log('Registrando...');
-    console.log(this.user.nombre);
+    console.log(this.user);
 
     this.usuarioService.registrarUsuario(this.user).subscribe(
-      (res)=>{
+      (res) => {
         console.log(res);
+        alert('¡Usuario registrado con éxito!');
       },
-      (err)=>{
-        console.log(err);
+      (err) => {
+        console.error(err);
+        alert('Hubo un error al registrar el usuario.');
       }
     );
-
   }
-
 }
